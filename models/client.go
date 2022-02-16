386648f262c8d07d45bfeb39d78165163bf3fff4
Nam-Ki-Bok <nkb7714@naver.com>
2022-02-16 18:03:13 +0900

    client 유효성 검사 추가

--- models/client.go
+++ models/client.go
@@ -1,16 +1,28 @@
 package models
 
+import (
+	"github.com/gin-gonic/gin"
+	"infradev-practice/Wade/OAuth2.0-server/database/maria"
+	"infradev-practice/Wade/OAuth2.0-server/utils"
+)
+
 type OauthClients struct {
-	ClientID     string //`gorm:"varchar(80);primary_key"`
-	ClientSecret string //`gorm:"varchar(80);"`
-	ClientIP     string //`gorm:"varchar(16);"`
-	GrantType    string //`gorm:"varchar(80);"`
-	Scope        string //`gorm:"varchar(1600);"`
+	ClientID     string
+	ClientSecret string
+	ClientIP     string
+	GrantType    string
+	Scope        string
 }
 
 // NewClient create to client model instance
-func NewClient() *OauthClients {
-	return &OauthClients{}
+func NewClient(c *gin.Context) *OauthClients {
+	id, secret := utils.BindClientInfo(c)
+
+	return &OauthClients{
+		ClientID:     id,
+		ClientSecret: secret,
+		ClientIP:     c.ClientIP(),
+	}
 }
 
 func (c *OauthClients) GetClientID() string {
@@ -31,4 +43,18 @@ func (c *OauthClients) GetGrantType() string {
 
 func (c *OauthClients) GetScope() string {
 	return c.Scope
+}
+
+// IsValid check client in ACL
+func (c *OauthClients) IsValid() bool {
+	err := maria.DB.Where("client_id = ?", c.ClientID).
+		Where("client_secret = ?", c.ClientSecret).
+		Where("client_ip = ?", c.ClientIP).
+		Find(c).Error
+
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
 }
