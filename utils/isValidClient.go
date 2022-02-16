d9459a49070ef5bb493bacc2c5ce9d5432c2aefa
Nam-Ki-Bok <nkb7714@naver.com>
2022-02-16 16:01:47 +0900

    logic 변경

--- utils/isValidClient.go
+++ utils/isValidClient.go
@@ -1,10 +1,15 @@
 package utils
 
+import (
+	"infradev-practice/Wade/OAuth2.0-server/database/maria"
+	"infradev-practice/Wade/OAuth2.0-server/models"
+)
+
 func IsValidClient(id, pw string) bool {
-	err := mariaDB.Where("client_id = ?", id).
+	err := maria.DB.Where("client_id = ?", id).
 		Where("client_secret = ?", pw).
 		Where("server_ip = ?", "1.1.1.1"). // 1.1.1.1 -> c.ClientIP()
-		Find(responseClient).Error
+		Find(&models.OauthClients{}).Error
 
 	if err != nil {
 		return false
