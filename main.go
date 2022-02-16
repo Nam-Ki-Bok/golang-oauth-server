0ceba326d679e93ce925745a4826e0d6ed94d257
Nam-Ki-Bok <nkb7714@naver.com>
2022-02-16 20:46:24 +0900

    refactoring

--- main.go
+++ main.go
@@ -1,145 +1,53 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/go-oauth2/oauth2/v4/errors"
 	"github.com/go-oauth2/oauth2/v4/generates"
 	"github.com/go-oauth2/oauth2/v4/manage"
-	"github.com/go-oauth2/oauth2/v4/models"
 	"github.com/go-oauth2/oauth2/v4/server"
 	"github.com/go-oauth2/oauth2/v4/store"
-	"github.com/go-redis/redis/v7"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
+
 	"golang.org/x/net/context"
-	"golang.org/x/oauth2/clientcredentials"
+	"infradev-practice/Wade/OAuth2.0-server/database/maria"
+	"infradev-practice/Wade/OAuth2.0-server/database/redis"
+	"infradev-practice/Wade/OAuth2.0-server/models"
 	"infradev-practice/Wade/OAuth2.0-server/utils"
 	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 )
 
 var (
 	manager     = manage.NewDefaultManager()
 	srv         = server.NewServer(server.NewConfig(), manager)
 	clientStore = store.NewClientStore()
-
-	requestClient  *PublicApiInfo
-	responseClient *OauthClients
-	clientConfig   *clientcredentials.Config
-
-	mariaDB *gorm.DB
-	redisDB *redis.Client
 )
-
-type OauthClients struct {
-	ClientID     string `gorm:"varchar(80);primary_key" json:"client_id"`
-	ClientSecret string `gorm:"varchar(80);" json:"client_secret"`
-	ClientIP     string `gorm:"varchar(16);" json:"client_ip"`
-	GrantTypes   string `gorm:"varchar(80);" json:"grant_types"`
-	Scope        string `gorm:"varchar(1600);" json:"scope"`
-}
-
-type OauthUsers struct {
-	ID    string `gorm:"varchar(80);primary_key"`
-	Phone string `gorm:"varchar(80)"`
-	Email string `gorm:"varchar(80)"`
-}
-
-type PublicApiInfo struct {
-	ClientID     string `json:"client_id" form:"client_id"`
-	ClientSecret string `json:"client_secret" form:"client_secret"`
-	UserID       string `json:"user_id" form:"user_id"`
-}
-
-type AuthorizationInfo struct {
-	ClientID    string    `json:"client_id"`
-	AccessToken string    `json:"access_token"`
-	Scope       []string  `json:"scope"`
-	ExpiresIn   time.Time `json:"expires_in"`
-}
 
 func init() {
 	initManager()
-	initServer()
-	initDatabase()
+
+	maria.Connect()
+	redis.Connect()
 }
 
 func main() {
 	r := gin.Default()
 
-	r.POST("/oauth/token", func(c *gin.Context) {
+	r.GET("/token", publicApiRequestHandler)
+	r.POST("/generate/token", func(c *gin.Context) {
 		err := srv.HandleTokenRequest(c.Writer, c.Request)
 		if err != nil {
-			http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+			utils.ReturnError(c, http.StatusInternalServerError, "Failed to generate a Token")
 		}
 	})
-
-	r.POST("/user/token", publicApiRequestHandler)
-
-	r.GET("/user/info/:id", userInfoHandler)
 
 	log.Fatal(r.Run(":9096"))
 }
 
 func initManager() {
 	manager.SetClientTokenCfg(manage.DefaultClientTokenCfg)
-
 	manager.MustTokenStorage(store.NewMemoryTokenStore())
-
 	manager.MapAccessGenerate(generates.NewAccessGenerate())
-
 	manager.MapClientStorage(clientStore)
-}
-
-func initServer() {
-	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
-		log.Println("Internal Error:", err.Error())
-		return
-	})
-
-	srv.SetResponseErrorHandler(func(re *errors.Response) {
-		log.Println("Response Error:", re.Error.Error())
-	})
-}
-
-func initDatabase() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	mariaDB, err = gorm.Open("mysql", os.Getenv("DATA_CONNECTION_INFO"))
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	mariaDB.LogMode(true)
-
-	redisDB = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-}
-
-func setClientStore() {
-	_ = clientStore.Set(responseClient.ClientID, &models.Client{
-		ID:     responseClient.ClientID,
-		Secret: responseClient.ClientSecret,
-		Domain: responseClient.ClientIP,
-	})
-}
-
-func setClientConfig() {
-	clientConfig = new(clientcredentials.Config)
-	scopes := utils.SplitScope(responseClient.Scope)
-
-	clientConfig.ClientID = responseClient.ClientID
-	clientConfig.ClientSecret = responseClient.ClientSecret
-	clientConfig.TokenURL = "http://localhost:9096/oauth/token"
-	clientConfig.Scopes = scopes
 }
 
 func publicApiRequestHandler(c *gin.Context) {
@@ -150,70 +58,33 @@ func publicApiRequestHandler(c *gin.Context) {
 		}
 	}()
 
-	clientID, clientSecret := utils.BindRequestClient(c)
-
-	if isValidClient(clientID, clientSecret) {
-		log.Println("durldasdasd")
-		setClientConfig()
-		setClientStore()
-	} else {
-		log.Println("durlㅇㄴㄹㅁㅈㄷㄹd")
-		c.JSON(500, gin.H{
-			"message": "invalid client",
-		})
-		return
-	}
-
-	token, err := clientConfig.Token(context.Background())
-	if err != nil {
-		http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	authorizationInfo := &AuthorizationInfo{
-		ClientID:    responseClient.ClientID,
-		AccessToken: token.AccessToken,
-		Scope:       clientConfig.Scopes,
-		ExpiresIn:   token.Expiry,
-	}
-	saveAuthorizationInfo(authorizationInfo)
-
-	c.JSON(200, token)
-}
-
-func userInfoHandler(c *gin.Context) {
-	userID := c.Param("id")
-
-	tokenInfo, err := srv.ValidationBearerToken(c.Request)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-
-	if !isValidScope(tokenInfo.GetScope(), "write") {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": "invalid scope",
-		})
-		return
-	}
-
-	responseUser := new(OauthUsers)
-	mariaDB.Where("id = ?", userID).Find(responseUser)
-	c.JSON(http.StatusOK, responseUser)
-}
-
-func saveAuthorizationInfo(authorizationInfo *AuthorizationInfo) {
-	data, _ := json.Marshal(authorizationInfo)
-	redisDB.Set(authorizationInfo.ClientID, data, authorizationInfo.ExpiresIn.Sub(time.Now()))
-}
-
-func isValidScope(userScope string, apiScope string) bool {
-	for _, scope := range strings.Split(userScope, " ") {
-		if apiScope == scope {
-			return true
+	client := models.NewClient(c)
+	if client.IsValid() {
+		err := SaveClientStore(client)
+		if err != nil {
+			utils.ReturnError(c, http.StatusInternalServerError, "Failed to save to client store")
 		}
+	} else {
+		utils.ReturnError(c, http.StatusUnauthorized, "Invalid client")
 	}
-	return false
+
+	config := client.GetConfig()
+	token, err := config.Token(context.Background())
+	if err != nil {
+		utils.ReturnError(c, http.StatusInternalServerError, "Failed to generate a token")
+	}
+
+	authInfo := models.NewAuthInfo(client, token)
+	authInfo.SaveRedis()
+
+	c.JSON(200, authInfo)
+}
+
+func SaveClientStore(c *models.OauthClients) error {
+	err := clientStore.Set(c.GetClientID(), c.GetSaveModel())
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
 }
