2e6cec4e7ef8e5d1759d195929a882f48786a10e
Nam-Ki-Bok <nkb7714@naver.com>
2022-02-16 20:43:48 +0900

    redis 저장용 model 생성

--- models/authInfo.go
+++ models/authInfo.go
@@ -1,1 +1,29 @@
 package models
+
+import (
+	"encoding/json"
+	"golang.org/x/oauth2"
+	"infradev-practice/Wade/OAuth2.0-server/database/redis"
+	"time"
+)
+
+type AuthInfo struct {
+	ClientID    string    `json:"client_id"`
+	AccessToken string    `json:"access_token"`
+	Scope       []string  `json:"scope"`
+	ExpiresIn   time.Time `json:"expires_in"`
+}
+
+func NewAuthInfo(c *OauthClients, t *oauth2.Token) *AuthInfo {
+	return &AuthInfo{
+		ClientID:    c.ClientID,
+		AccessToken: t.AccessToken,
+		Scope:       c.Config.Scopes,
+		ExpiresIn:   t.Expiry,
+	}
+}
+
+func (a *AuthInfo) SaveRedis() {
+	data, _ := json.Marshal(a)
+	redis.DB.Set(a.ClientID, data, a.ExpiresIn.Sub(time.Now()))
+}
