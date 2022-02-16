118c32f617282b54bb6142e14a215c0e43d14bea
Nam-Ki-Bok <nkb7714@naver.com>
2022-02-16 11:52:25 +0900

    func 설명 주석 추가

--- utils/ReturnError.go
+++ utils/ReturnError.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ReturnError Return various errors to JSON
 func ReturnError(c *gin.Context, code int, msg string) {
 	c.JSON(code, gin.H{
 		"err": msg,
