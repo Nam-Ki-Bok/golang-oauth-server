9e1b7a8cdb8cdcc757bafc6bf9d2bf0c74edabb6
Nam-Ki-Bok <nkb7714@naver.com>
2022-02-17 01:05:45 +0900

    token handler 분리

--- token/Generate.go
+++ token/Generate.go
@@ -1,1 +1,14 @@
 package token
+
+import (
+	"github.com/gin-gonic/gin"
+	"infradev-practice/Wade/OAuth2.0-server/utils"
+	"net/http"
+)
+
+func Generate(c *gin.Context) {
+	err := srv.HandleTokenRequest(c.Writer, c.Request)
+	if err != nil {
+		utils.ReturnError(c, http.StatusInternalServerError, "Failed to generate a Token")
+	}
+}
--- token/config.go
+++ token/config.go
@@ -1,1 +1,24 @@
 package token
+
+import (
+	"github.com/go-oauth2/oauth2/v4/generates"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-oauth2/oauth2/v4/store"
+	"infradev-practice/Wade/OAuth2.0-server/models"
+)
+
+var (
+	manager = manage.NewDefaultManager()
+	srv     = server.NewServer(server.NewConfig(), manager)
+	cs      = store.NewClientStore()
+
+	authInfo = new(models.AuthInfo)
+)
+
+func init() {
+	manager.SetClientTokenCfg(manage.DefaultClientTokenCfg)
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+	manager.MapAccessGenerate(generates.NewAccessGenerate())
+	manager.MapClientStorage(cs)
+}
