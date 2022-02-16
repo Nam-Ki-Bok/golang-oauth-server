982f32f5303d077eaadc0a3baad606323d7bf059
Nam-Ki-Bok <nkb7714@naver.com>
2022-02-16 20:43:14 +0900

    나머지 필요한 자료 저장

--- models/client.go
+++ models/client.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"golang.org/x/oauth2/clientcredentials"
 	"infradev-practice/Wade/OAuth2.0-server/database/maria"
 	"infradev-practice/Wade/OAuth2.0-server/utils"
 )
@@ -12,6 +14,9 @@ type OauthClients struct {
 	ClientIP     string
 	GrantType    string
 	Scope        string
+
+	Config    *clientcredentials.Config
+	SaveModel *models.Client
 }
 
 // NewClient create to client model instance
@@ -41,8 +46,16 @@ func (c *OauthClients) GetGrantType() string {
 	return c.GrantType
 }
 
-func (c *OauthClients) GetScope() string {
-	return c.Scope
+func (c *OauthClients) GetScope() []string {
+	return c.Config.Scopes
+}
+
+func (c *OauthClients) GetConfig() *clientcredentials.Config {
+	return c.Config
+}
+
+func (c *OauthClients) GetSaveModel() *models.Client {
+	return c.SaveModel
 }
 
 // IsValid check client in ACL
@@ -55,6 +68,31 @@ func (c *OauthClients) IsValid() bool {
 	if err != nil {
 		return false
 	} else {
+		c.SetConfig()
+		c.SetSaveModel()
 		return true
 	}
+}
+
+// SetConfig set clientcredentials.Config struct
+func (c *OauthClients) SetConfig() {
+	scopes := utils.SplitScope(c.Scope)
+
+	cfg := new(clientcredentials.Config)
+	cfg.ClientID = c.GetClientID()
+	cfg.ClientSecret = c.GetClientSecret()
+	cfg.TokenURL = "http://localhost:9096/generate/token"
+	cfg.Scopes = scopes
+
+	c.Config = cfg
+}
+
+// SetSaveModel set model to store in the client store
+func (c *OauthClients) SetSaveModel() {
+	model := new(models.Client)
+	model.ID = c.GetClientID()
+	model.Secret = c.GetClientSecret()
+	model.Domain = c.GetClientIP()
+
+	c.SaveModel = model
 }
