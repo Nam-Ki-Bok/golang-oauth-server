c9b446137a57a201085eac840562a0603ac601cd
Nam-Ki-Bok <nkb7714@naver.com>
2022-03-05 22:36:48 +0900

    branch test

--- middleware/validateClient.go
+++ middleware/validateClient.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"infradev-practice/Wade/OAuth2.0-server/models"
 	"infradev-practice/Wade/OAuth2.0-server/utils"
@@ -10,6 +11,7 @@ import (
 
 func ValidateClient(c *gin.Context) {
 	models.InitClient(c)
+	fmt.Println("여기 변경이 메인엔 없어야지????")
 	if ok := models.Client.IsValid(); !ok {
 		utils.ReturnError(c, http.StatusUnauthorized, errors.New("invalid client"))
 	}
