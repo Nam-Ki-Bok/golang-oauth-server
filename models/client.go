5a67359e6f4e3448a127f21405a0e61ccb6606e9
Nam-Ki-Bok <nkb7714@naver.com>
2022-03-06 19:20:11 +0900

    client 지역변수로 변경

--- models/client.go
+++ models/client.go
@@ -1,13 +1,10 @@
 package models
 
 import (
-	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-oauth2/oauth2/v4/models"
 	"golang.org/x/oauth2/clientcredentials"
 	"infradev-practice/Wade/OAuth2.0-server/database/maria"
-	"infradev-practice/Wade/OAuth2.0-server/utils"
-	"net/http"
 	"os"
 )
 
@@ -22,41 +19,19 @@ type Clients struct {
 	SaveModel *models.Client            `gorm:"-"`
 }
 
-var (
-	Client *Clients
-)
+func NewClient(c *gin.Context) *Clients {
+	client := new(Clients)
 
-func InitClient(c *gin.Context) {
-	id, secret, ok := c.Request.BasicAuth()
-	if !ok {
-		utils.ReturnError(http.StatusUnauthorized, errors.New("authentication information error"))
-	}
+	id := c.GetString("client_id")
+	secret := c.GetString("client_secret")
+	maria.DB.Where("client_id = ?", id).
+		Where("client_secret = ?", secret).
+		Where("client_ip = ?", c.ClientIP()).Find(&client)
 
-	if err := utils.CheckID(id); err != nil {
-		utils.ReturnError(http.StatusUnauthorized, err)
-	}
+	client.SetConfig()
+	client.SetSaveModel()
 
-	if err := utils.CheckSecret(secret); err != nil {
-		utils.ReturnError(http.StatusUnauthorized, err)
-	}
-
-	Client = &Clients{
-		ClientID:     id,
-		ClientSecret: utils.GenerateSHA256(secret),
-		ClientIP:     c.ClientIP(),
-	}
-}
-
-// IsValid check client in ACL
-func (c *Clients) IsValid() bool {
-	result := maria.DB.Where("client_id = ?", c.ClientID).
-		Where("client_secret = ?", c.ClientSecret).
-		Where("client_ip = ?", c.ClientIP).Find(c)
-
-	if result.RowsAffected == 0 {
-		return false
-	}
-	return true
+	return client
 }
 
 // SetConfig set clientcredentials.Config struct
@@ -76,44 +51,4 @@ func (c *Clients) SetSaveModel() {
 		Secret: c.ClientSecret,
 		Domain: c.ClientIP,
 	}
-}
-
-func (c *Clients) GetClientID() string {
-	return c.ClientID
-}
-
-func (c *Clients) SetClientID(id string) {
-	c.ClientID = id
-}
-
-func (c *Clients) GetClientSecret() string {
-	return c.ClientSecret
-}
-
-func (c *Clients) SetClientSecret(secret string) {
-	c.ClientSecret = secret
-}
-
-func (c *Clients) GetClientIP() string {
-	return c.ClientIP
-}
-
-func (c *Clients) SetClientIP(ip string) {
-	c.ClientIP = ip
-}
-
-func (c *Clients) GetGrantType() string {
-	return c.GrantType
-}
-
-func (c *Clients) GetScope() []string {
-	return c.Config.Scopes
-}
-
-func (c *Clients) GetConfig() *clientcredentials.Config {
-	return c.Config
-}
-
-func (c *Clients) GetSaveModel() *models.Client {
-	return c.SaveModel
 }
